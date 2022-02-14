03d1688c7400d6d928c17ff1e201fc4f4a2a6370
Danilo Vargas <danilovargas.01@gmail.com>
2022-02-13 21:12:27 -0500

    Fixed bug in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 install_requires = [
     'starlette==0.14.2',
-    'meliodas==1.1.0',
+    'meliodas==1.1.1',
     'rocketchat==1.0.1',
     'hawk==1.0.8'
 ]
@@ -18,12 +18,12 @@ test_require = [
 
 setup(
     name='security',
-    version='1.0.5',
+    version='1.0.6',
     packages=['security'],
     install_requires=install_requires,
     author='Danilo Vargas',
     dependency_links=[
-        'https://github.com/dasouch/meliodas.git@v1.1.0#egg=meliodas',
+        'https://github.com/dasouch/meliodas.git@v1.1.1#egg=meliodas',
         'https://github.com/dasouch/rocketchat-singleton.git@v1.0.1#egg=rocketchat',
         'https://github.com/dasouch/hawk.git@v1.0.8#egg=hawk'
     ]
